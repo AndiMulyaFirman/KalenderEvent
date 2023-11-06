dd74b18ab7047456833e100c0dd1bb26898b46d3
andimulyafirman <andimulya1310@gmail.com>
2023-11-06 17:44:38 +0700

    Progress 06/11/2023

--- app/src/main/java/com/kominfo/kalenderevent/presentation/main/MainActivity.kt
+++ app/src/main/java/com/kominfo/kalenderevent/presentation/main/MainActivity.kt
@@ -1,13 +1,75 @@
 package com.kominfo.kalenderevent.presentation.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.kominfo.kalenderevent.R
+import com.kominfo.kalenderevent.presentation.home.HomeFragment
+import com.kominfo.kalenderevent.presentation.profil.ProfilFragment
+import com.kominfo.kalenderevent.presentation.seacrh.SearchFragment
+import com.kominfo.kalenderevent.presentation.tambahEvent.TambahEventFragment
+import com.kominfo.kalenderevent.presentation.tiketSaya.TiketSayaFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private var doubleBackToExitPressedOnce = false
+    private val TOAST_DURATION = 2000 // Durasi toast dalam milidetik (misalnya 2000ms = 2 detik)
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Tekan lagi untuk keluar", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, TOAST_DURATION.toLong())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.miHome -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.miTiketSaya -> {
+                    replaceFragment(TiketSayaFragment())
+                    true
+                }
+                R.id.miProfile -> {
+                    replaceFragment(ProfilFragment())
+                    true
+                }
+
+
+                else -> false
+            }
+        }
+
+
+        // Set the default selected menu item
+        bottomNavigationView.selectedItemId = R.id.miHome
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 }
--- app/src/main/java/com/kominfo/kalenderevent/presentation/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/com/kominfo/kalenderevent/presentation/splashscreen/SplashscreenActivity.kt
@@ -1,12 +1,49 @@
 package com.kominfo.kalenderevent.presentation.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
-import com.kominfo.kalenderevent.R
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.kominfo.kalenderevent.databinding.ActivitySplashscreenBinding
+import com.kominfo.kalenderevent.presentation.main.MainActivity
 
 class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashscreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Atur nilai awal alpha menjadi 0
+        binding.tvNameApp.alpha = 0f
+        binding.tvDescApp.alpha = 0f
+
+        playAnimation()
+
+        // Handler untuk berpindah ke MainActivity setelah animasi selesai
+        binding.root.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2500)
+    }
+
+    private fun playAnimation() {
+        val title1 = ObjectAnimator.ofFloat(binding.tvNameApp, View.ALPHA, 1f).setDuration(1000)
+        val title2 = ObjectAnimator.ofFloat(binding.tvDescApp, View.ALPHA, 1f).setDuration(1500)
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(title1, title2)
+        animatorSet.start()
     }
 }
